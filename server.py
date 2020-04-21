a0eb25a7119c43f2e12673506b4675806f32d60f
Vibhu <43749855+vibhu18116@users.noreply.github.com>
2020-04-21 19:53:41 +0530

    Minor Updates in Design

--- server.py
+++ server.py
@@ -8,7 +8,7 @@ connection = MySQL_Conn.getInstance('healthkart', 'root')
 def selectdept():
 	all_depts = []
 	if connection.connect():
-		depts = connection.execute("select DepartmentName from Departments")
+		depts = connection.execute("select DepartmentName from departments")
 		for i in depts:
 			all_depts.append(i[0])
 	return render_template("appointmentdept.html", depts = all_depts)
@@ -18,20 +18,23 @@ def selectdept():
 def selectdoctor():
 	dept = request.form['submit_button']
 	doctors = []
+	doctor_id = []
 	if connection.connect():
-		docs = connection.execute("select DoctorName from Doctors where DepartmentName = '%s'" %dept)
+		docs = connection.execute("select DoctorID, DoctorName from doctors where DepartmentName = '%s'" %dept)
 		for i in docs:
-			doctors.append(i[0])
-	return render_template("appointmentdoctor.html", doctors = doctors, dept = dept)
+			doctors.append("Dr. " + i[1])
+			doctor_id.append(i[0])
+	return render_template("appointmentdoctor.html", doctors = doctors, dept = dept, dID = doctor_id)
 
 
 @app.route("/patients/appointments/getschedule", methods = ['GET', 'POST'])
 def getschedule():
 	appointment_details = request.form['submit_button']
 	appointment_details = appointment_details.split(' ')
-	dept=appointment_details[0]
-	doctor=appointment_details[1]
-	return render_template("bookschedule.html", dept = dept, doctor = doctor)
+	dept = appointment_details[0]
+	doctor = " ".join(appointment_details[1:-1])
+	did = appointment_details[-1]
+	return render_template("bookschedule.html", dept = dept, doctor = doctor, dID = did)
 
 
 if __name__ == "__main__":
